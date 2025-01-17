fe4d98185b66cfb969e0ac5cf8ce0ade66c150a8
Ajit <ajit.megaproject@gmail.com>
2025-01-17 10:53:28 +0900

    First push after chapter 5 completion

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Page() {
+  return (
+    <div>
+      <p>Customers Page</p>
+    </div>
+  );
+}
+
+export default Page;
---
+++ app/dashboard/refunds/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>Refunds</div>;
+}
+
+export default page;
